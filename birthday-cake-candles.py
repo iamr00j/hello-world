4212cdd86951426fdfa5b94bfe86ff78c00f6d23
iamr00j <42660439+iamr00j@users.noreply.github.com>
2018-09-06 15:34:24 +1000

    Create birthday-cake-candles.py
    Function Description
    
    Complete the function birthdayCakeCandles in the editor below. It must return an integer representing the number of candles she can blow out.
    
    birthdayCakeCandles has the following parameter(s):
    
    ar: an array of integers representing candle heights
    Input Format
    
    The first line contains a single integer, n, denoting the number of candles on the cake. 
    The second line contains n space-separated integers, where each integer i describes the height of candle i.
    
    Output Format
    
    Print the number of candles that can be blown out on a new line.

---
+++ birthday-cake-candles.py
@@ -0,0 +1,28 @@
+#https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the birthdayCakeCandles function below.
+def birthdayCakeCandles(ar):
+    number = max(ar)
+    counter = ar.count(number)
+    return counter
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
